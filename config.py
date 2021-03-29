60aaf82f326bf76fcea9a8d0a3ce74accab803bf
jun1116 <52767291+jun1116@users.noreply.github.com>
2021-03-30 01:43:37 +0900

    PostgresSQL로 ElephantSQL 연결 완료

--- config.py
+++ config.py
@@ -1,14 +1,3 @@
-# # config.py
-# db = {
-#     'user'     : 'root',		# 1)
-#     'password' : '0000',		# 2)
-#     'host'     : 'localhost',	# 3)
-#     'port'     : 3306,			# 4)
-#     'database' : 'kokoa'		# 5)
-# }
-# SQLALCHEMY_DATABASE_URI: postgresql://username:password@localhost:5432/dbname
-# DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
-
 class Config(object):
     DEBUG = False
     TESTING = False
@@ -17,8 +6,19 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite+pysqlite:///dev_db.sqlite3'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite+pysqlite:///dev_db.sqlite3'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+        url="arjuna.db.elephantsql.com",user='puhxmtsx',pw='FlicJEXwJK-YRVaVQ9YcUrw43i0zGqHh',db='puhxmtsx')
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite+pysqlite:///prod_db.sqlite3'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite+pysqlite:///prod_db.sqlite3'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+        url="arjuna.db.elephantsql.com",user='puhxmtsx',pw='FlicJEXwJK-YRVaVQ9YcUrw43i0zGqHh',db='puhxmtsx')
+
+# 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+#     url="arjuna.db.elephantsql.com"
+#     ,user='puhxmtsx'
+#     ,pw='FlicJEXwJK-YRVaVQ9YcUrw43i0zGqHh'
+#     ,db='puhxmtsx'
+#     )
